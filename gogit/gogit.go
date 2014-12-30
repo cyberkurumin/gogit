037f2d473ef86ceb2fef8cda71bdf0b3b5ca8afe
Jorge GOMES <jorge@blondeel-gomes.net>
2014-12-30 02:30:11 +0100

    Testing commit to try the current status list

--- gogit/gogit.go
+++ gogit/gogit.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/libgit2/git2go"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-
+	path := "/home/jorge/go/src/github.com/gogit"
+	repo, err := git.InitRepository(path, false)
+	if err != nil {
+		os.Exit(1)
 	}
-	args := os.Args[1:]
+
+	opts := &git.StatusOptions{}
+	opts.Show = git.StatusShowIndexAndWorkdir
+	opts.Flags = git.StatusOptIncludeUntracked | git.StatusOptRenamesHeadToIndex | git.StatusOptSortCaseSensitively
+
+	statusList, err := repo.StatusList(opts)
+
+	entry, err := statusList.ByIndex(0)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("%v\n", entry.IndexToWorkdir)
+	fmt.Printf("%v\n", entry.HeadToIndex)
 }
